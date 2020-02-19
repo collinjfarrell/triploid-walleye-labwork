733335a101c703444220890cb5fc6355ddce6f0d
collinjfarrell <58794225+collinjfarrell@users.noreply.github.com>
2020-02-19 08:35:05 -0700

    Create radii_data_combine.R

---
+++ Images/otolith images/radii_data_combine.R
@@ -0,0 +1,21 @@
+library(RFishBC)
+library(tidyverse)
+library(magrittr)
+
+fns <- listFiles("rds")
+
+dfrad_wide <- combineData(fns, formatOut = "wide")
+
+dfrad_wide %<>% mutate(rep = 1,
+                  fishid = str_sub(id,3,str_length(id)),
+                  light_type = str_sub(id,1,1)) %>%
+            select(fishid,id, light_type, reading, rep,everything())
+
+dfrad_long <- combineData(fns)
+
+
+ggplot(dfrad,aes(agecap,radcap))+
+  geom_point()
+
+ggplot(dfrad_long,aes(as.factor(ann),rad))+
+  geom_boxplot()
