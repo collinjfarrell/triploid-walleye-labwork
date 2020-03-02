dd4345a2a3c12e47fe97835169a43bf82f0f1233
collinjfarrell <58794225+collinjfarrell@users.noreply.github.com>
2020-03-02 08:54:50 -0700

    Update for MacOS

--- Images/otolith images/RFishBC_2.R
+++ Images/otolith images/RFishBC_2.R
@@ -11,11 +11,12 @@ edge_setting <- FALSE
 #If fish was caught prior to May 1, set to TRUE
 #If fish was caught on or after May 1, set to FALSE
 
-reader_initials <- "LEL" #Use your initials
+reader_initials <- "CJF" #Use your initials
 read_num <- "1" #change if you read individual more than one
 
-size_window <- 12 #controls window size
+size_window <- 10 #controls window size
 color_id <- "grey70" #controls color of ID text
+devicetype <- "X11" # "X11" for MacOS, "windows" for Windows
 
 scale_bar <- TRUE     # TRUE = scale bar present
 scale_bar_length <- 1 #length of scale bar (should be 1mm)
@@ -37,5 +38,6 @@ digitizeRadii(imgs,
               scaleBar = scale_bar,
               scaleBarLength = scale_bar_length,
               scaleBarUnits = scale_bar_unit,
-              col.sel = color_selected_points
+              col.sel = color_selected_points,
+              deviceType = devicetype
               )
