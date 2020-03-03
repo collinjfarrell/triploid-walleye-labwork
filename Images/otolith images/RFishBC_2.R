e433c8ba0ed86d639d5e86c0b5a616bb61d21c90
lillian legg <lillianlegg@gmail.com>
2020-03-03 12:21:33 -0700

    otolith ageing

--- Images/otolith images/RFishBC_2.R
+++ Images/otolith images/RFishBC_2.R
@@ -11,13 +11,13 @@ edge_setting <- FALSE
 #If fish was caught prior to May 1, set to TRUE
 #If fish was caught on or after May 1, set to FALSE
 
-reader_initials <- "CJF" #Use your initials
-read_num <- "2" #change if you read individual more than one
+reader_initials <- "LEL" #Use your initials
+read_num <- "1" #change if you read individual more than one
 
 size_window <- 14 #controls window size
 color_id <- "grey70" #controls color of ID text
 devicetype <- "X11" # "X11" for MacOS, "windows" for Windows
-X11.options(xpos = 1860) # Controls the x-position of the new window for macOS
+# X11.options(xpos = 1860) # Controls the x-position of the new window for macOS
 
 scale_bar <- TRUE     # TRUE = scale bar present
 scale_bar_length <- 1 #length of scale bar (should be 1mm)
