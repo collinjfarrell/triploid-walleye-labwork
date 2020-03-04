824b2f610f83fd3e323bbeff1a4979bbb1f206d3
Collin J. Farrell <collin.farrell@colostate.edu>
2020-03-03 18:44:50 -0700

    Update radii_data_combine.R

--- Images/otolith images/radii_data_combine.R
+++ Images/otolith images/radii_data_combine.R
@@ -7,19 +7,31 @@ fns <- listFiles("rds")
 
 dfrad_wide <- combineData(fns, formatOut = "wide")
 
-dfrad_wide %<>% mutate(rep = 1,
+dfrad_wide %<>% mutate(rep = str_sub(reading,5,5),
                   fishid = str_sub(id,3,str_length(id)),
-                  light_type = str_sub(id,1,1)) %>%
-            select(fishid,id, light_type, reading, rep,everything())
+                  light_type = str_sub(id,1,1),
+                  waterbody = str_sub(fishid,1,3)) %>%
+            select(fishid,id, light_type, reading, rep,everything()) %>%
+                mutate(light_type = as.factor(light_type),
+                       reading = as.factor(reading),
+                       waterbody = as.factor(waterbody))
 
 dfrad_long <- combineData(fns)
+
+dfrad_long <- dfrad_wide %>%
+  pivot_longer(cols = starts_with("rad"),
+               names_to = "ann",
+               names_prefix = "rad",
+               values_to = "rad",
+               values_drop_na = TRUE)
 
 
 ggplot(dfrad_wide,aes(agecap,radcap,color = reading))+
   geom_jitter()
 
 ggplot(dfrad_long,aes(as.factor(ann),rad,fill = reading))+
-  geom_boxplot()
+  geom_boxplot() +
+  facet_wrap(.~waterbody)
 
 
 
