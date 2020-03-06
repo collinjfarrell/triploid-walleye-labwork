ca36e8ebf72de5745a84c35080d81feeb4af203a
Collin J. Farrell <collin.farrell@colostate.edu>
2020-03-05 17:39:16 -0700

    added age_bias plots comps

--- Images/otolith images/radii_data_combine.R
+++ Images/otolith images/radii_data_combine.R
@@ -23,7 +23,8 @@ dfrad_long <- dfrad_wide %>%
                names_to = "ann",
                names_prefix = "rad",
                values_to = "rad",
-               values_drop_na = TRUE)
+               values_drop_na = TRUE) %>%
+  mutate(waterbody = str_sub(id,3,5))
 
 
 ggplot(dfrad_wide,aes(agecap,radcap,color = reading))+
@@ -31,28 +32,28 @@ ggplot(dfrad_wide,aes(agecap,radcap,color = reading))+
 
 ggplot(dfrad_long,aes(as.factor(ann),rad,fill = reading))+
   geom_boxplot() +
-  facet_wrap(.~waterbody)
+  facet_wrap(light_type~waterbody)
 
 
 
 age_comp <- dfrad_wide %>%
-  select(fishid,light_type,reading,agecap) %>%
-  pivot_wider(id_cols = fishid,names_from=reading,values_from = agecap)
+  select(id,light_type,reading,agecap) %>%
+  pivot_wider(id_cols = id,names_from=reading,values_from = agecap)
 
 # Compare All 
-age_precision <- agePrecision(~CJF_1+CJF_2 + JRS_1 + LEL_1,data=age_comp)
+age_precision <- agePrecision(~CJF+CJF_1 + JRS_1 + LEL_1,data=age_comp)
 summary(age_precision,what="precision")
 summary(age_precision,what="detail")
 
 
 # Self Comparison (CJF - CJF)
-age_precision_cjf <- agePrecision(~CJF_1 + CJF_2,data=age_comp)
+age_precision_cjf <- agePrecision(~CJF + CJF_2,data=age_comp)
 summary(age_precision_cjf,what = "precision")
 summary(age_precision_cjf,what="difference")
 summary(age_precision_cjf,what="absolute difference")
 summary(age_precision_cjf,what="details")
 
-age_bias_cjf <- ageBias(CJF_1~CJF_2, data=age_comp)
+age_bias_cjf <- ageBias(CJF~CJF_2, data=age_comp)
 plot(age_bias_cjf)
 plotAB(age_bias_cjf,what="numbers")
 
@@ -60,11 +61,23 @@ plotAB(age_bias_cjf,what="numbers")
 age_precision_jrs <- agePrecision(~CJF_1 + JRS_1, data = age_comp)
 summary(age_precision_jrs,what="precision")
 
+age_bias_jrs <- ageBias(JRS_1 ~ CJF_1, data=age_comp)
+plot(age_bias_jrs)
+plotAB(age_bias_jrs,what="numbers")
+
 # CJF to LEL (Lillian Legg)
 age_precision_lel <- agePrecision(~CJF_1 + LEL_1, data = age_comp)
 summary(age_precision_lel,what="precision")
+
+age_bias_lel <- ageBias(LEL_1 ~ CJF_1, data=age_comp)
+plot(age_bias_lel)
+plotAB(age_bias_lel,what="numbers")
 
 # CJF to MJS (Matt Swerdfeger)
 age_precision_mjs <- agePrecision(~CJF_1 + MJS_1, data = age_comp)
 summary(age_precision_mjs,what="precision")
 summary(age_precision_mjs, what="symmetry")
+
+age_bias_mjs <- ageBias(MJS_1 ~ CJF_1, data=age_comp)
+plot(age_bias_mjs)
+plotAB(age_bias_mjs,what="numbers")
