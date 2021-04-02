1c45092ac8711ebf6f939e5c9af481a6de51cf6e
collinjfarrell <58794225+collinjfarrell@users.noreply.github.com>
2021-04-01 18:17:31 -0600

    aged

--- Images/otolith images/scripts/RFishBC_windows.R
+++ Images/otolith images/scripts/RFishBC_windows.R
@@ -13,13 +13,13 @@ edge_setting <- FALSE
 
 
 reader_initials <- "CJF" #Use your initials
-read_num <- "1" #change if you read individual more than one
+read_num <- "2" #change if you read individual more than one
 
 size_window <- 12 #controls window size
 color_id <- "grey70" #controls color of ID text
 devicetype <- "default" # "X11" for MacOS, "windows" for Windows
  #X11.options(xpos = 1860) # Controls the x-position of the new window for macOS (CJF_Crestmore = 1860)
-windows.options(xpos = 50, ypos = 50) # Controls x-position for new window for Windows10 (CJF_Wagar = 1051)
+windows.options(xpos = 1921, ypos = 0) # Controls x-position for new window for Windows10 (CJF_Wagar = 1051)
 scale_bar <- TRUE     # TRUE = scale bar present
 scale_bar_length <- 1 #length of scale bar (should be 1mm)
 scale_bar_unit <- "mm" #units of scale bar 
